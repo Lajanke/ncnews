177e6ce2be38b401ef04cdf57f7609bd20bf6d7a
Lajanke <l.j.kenny@hotmail.com>
2020-05-04 16:54:21 +0100

    Format data working with passing tests.

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -2,7 +2,6 @@ exports.formatDates = list => {
     return list.map(item => {
         const newItem = {...item};
         newItem.created_at = new Date(newItem.created_at);
-        console.log(newItem)
         return newItem;
     });
 };
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -24,13 +24,41 @@ describe('formatDates', () => {
   test('When passed an empty array returns an empty array', () => {
     expect(formatDates([])).toEqual([]);
   });
-  test('Changes timestamp to readable format', () => {
-    expect(formatDates([{created_at: 1511354163389}])).toEqual([{created_at: new Date(1511354163389)}])
-  })
+  test('Alters timestamp to readable format', () => {
+    expect(formatDates([{ created_at: 1511354163389 }])).toEqual([{ created_at: new Date(1511354163389) }])
+  });
+  test('Alters timestamp to JS object for multiple items in an array', () => {
+    output = formatDates(commentInput);
+    expect(output[0].created_at).toEqual(new Date(commentInput[0].created_at));
+    expect(output[1].created_at).toEqual(new Date(commentInput[1].created_at));
+  });
+  test('Does not alter any other data in the object', () => {
+    output = formatDates(commentInput);
+    expect(Object.keys(output[0])).toEqual(['body', 'belongs_to', 'created_by', 'votes', 'created_at'])
+    expect(output[0]).toEqual(
+      {
+        body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: 'butter_bridge',
+        votes: 16,
+        created_at: new Date(commentInput[0].created_at),
+      });
+  });
+  test('Does not mutate input array', () => {
+    formatDates(commentInput)
+    expect(commentInput[0]).toEqual(
+      {
+        body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: 'butter_bridge',
+        votes: 16,
+        created_at: 1511354163389,
+      });
+  });
 });
 
-describe('makeRefObj', () => {});
+describe('makeRefObj', () => { });
 
-describe('formatComments', () => {});
+describe('formatComments', () => { });
 
 
