c33334fc0e441ec8fa15dd5e33bddd2bb0bb342f
Lajanke <l.j.kenny@hotmail.com>
2020-05-04 23:35:27 +0100

    All util functions working and tests passing.

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,18 +1,27 @@
-exports.formatDates = list => {
+const formatDates = (list) => {
     return list.map(item => {
-        const newItem = {...item};
+        const newItem = { ...item };
         newItem.created_at = new Date(newItem.created_at);
         return newItem;
     });
 };
 
-exports.makeRefObj = list => {
+const makeRefObj = (list, param1, param2) => {
     const ref = list.reduce((newRef, article) => {
-        newRef[article['title']] = article['article_id'];
+        newRef[article[param1]] = article[param2];
         return newRef
     }, {});
     return ref;
 };
 
-exports.formatComments = (comments, articleRef) => {};
+const formatComments = (comments, articleRef) => {
+    const newComments = comments.map((comment) => {
+        const { belongs_to: key1, created_by: key2, ...restOfKeys } = comment;
+        return { article_id: articleRef[key1], author: key2, ...restOfKeys };
+    });
+    const formatTimestamp = formatDates(newComments)
+    return formatTimestamp;
+}
+
+module.exports = { formatDates, makeRefObj, formatComments, }
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -57,32 +57,98 @@ describe('formatDates', () => {
   });
 });
 
-describe.only('makeRefObj', () => {
+describe('makeRefObj', () => {
   const input = [
-    {'article_id': 1, 'title': 'A'},
-    {'article_id': 2, 'title': 'B'},
-    {'article_id': 3, 'title': 'C'},
+    { 'article_id': 1, 'title': 'A' },
+    { 'article_id': 2, 'title': 'B' },
+    { 'article_id': 3, 'title': 'C' },
   ];
-  const output = {'A': 1, 'B': 2, 'C': 3}
+  const output = { 'A': 1, 'B': 2, 'C': 3 }
   test('When passed an empty array, returns an empty object', () => {
     expect(makeRefObj([])).toEqual({});
   });
   test('Returns a lookup object with a key of title and a value of article_id', () => {
-    expect(makeRefObj([{'article_id': 1, 'title': 'A'}])).toEqual({'A': 1});
+    expect(makeRefObj([{ 'article_id': 1, 'title': 'A' }], 'title', 'article_id')).toEqual({ 'A': 1 });
   });
   test('Works for longer arrays', () => {
-    expect(makeRefObj(input)).toEqual(output);
+    expect(makeRefObj(input, 'title', 'article_id')).toEqual(output);
   });
   test('Does not mutate input', () => {
     makeRefObj(input)
     expect(input).toEqual([
-      {'article_id': 1, 'title': 'A'},
-      {'article_id': 2, 'title': 'B'},
-      {'article_id': 3, 'title': 'C'},
-    ])
-  })
+      { 'article_id': 1, 'title': 'A' },
+      { 'article_id': 2, 'title': 'B' },
+      { 'article_id': 3, 'title': 'C' },
+    ]);
+  });
 });
 
-describe('formatComments', () => { });
+describe('formatComments', () => {
+  const input = [{
+    body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+    belongs_to: "They're not exactly dogs, are they?",
+    created_by: 'butter_bridge',
+    votes: 16,
+    created_at: 1511354163389,
+  }];
+
+  const input2 = [{
+    body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+    belongs_to: "They're not exactly dogs, are they?",
+    created_by: 'butter_bridge',
+    votes: 16,
+    created_at: 1511354163389,
+  },
+  {
+    body:
+      'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+    belongs_to: 'Living in the shadow of a great man',
+    created_by: 'butter_bridge',
+    votes: 14,
+    created_at: 1479818163389,
+  },];
+
+  test('When passed an empty array returns an empty array', () => {
+    expect(formatComments([])).toEqual([]);
+  });
+  test('When passed a single comment object changes the keys', () => {
+    output = formatComments(input, { "They're not exactly dogs, are they?": 1 });
+    expect(output[0]).toHaveProperty('author');
+    expect(output[0]).toHaveProperty('article_id');
+    expect(output[0].article_id).toEqual(1);
+    expect(output[0]).not.toHaveProperty('belongs_to');
+    expect(output[0]).not.toHaveProperty('created_by');
+  });
+  test('Formats the timestamp on created_at property', () => {
+    output = formatComments(input, { "They're not exactly dogs, are they?": 1 });
+    expect(output[0].created_at).toEqual(new Date(input[0].created_at));
+  });
+  test('Works for multiple entries in an array', () => {
+    articleRef = {
+      "They're not exactly dogs, are they?": 1,
+      'Living in the shadow of a great man': 2,
+    };
+    output = formatComments(input2, articleRef);
+    output.forEach(comment => {
+      expect(comment).toHaveProperty('author');
+      expect(comment).toHaveProperty('article_id');
+      expect(comment).toHaveProperty('body');
+      expect(comment).toHaveProperty('votes');
+      expect(comment).toHaveProperty('created_at');
+      expect(comment).not.toHaveProperty('belongs_to');
+      expect(comment).not.toHaveProperty('created_by');
+    });
+  });
+  test('Does not mutate input array', () => {
+    formatComments(input2, articleRef);
+    expect(input2[1]).toEqual({
+      body: 'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+      belongs_to: 'Living in the shadow of a great man',
+      created_by: 'butter_bridge',
+      votes: 14,
+      created_at: 1479818163389,
+    });
+  });
+});
 
 
