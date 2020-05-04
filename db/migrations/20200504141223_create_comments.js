a25cdbf8b016399580361529cf93cff3fab4556f
Lajanke <l.j.kenny@hotmail.com>
2020-05-04 14:22:08 +0100

    Migration and table set up for comments.

---
+++ db/migrations/20200504141223_create_comments.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('comments', (commentsTable) => {
+        console.log('creating comments table');
+        console.log('creating comments tabele');
+        commentsTable.increments('comment_id').primary();
+        commentsTable.string('author');
+        commentsTable.foreign('author').references('users.username');
+        commentsTable.integer('article_id');
+        commentsTable.foreign('article_id').references('articles.article_id');
+        commentsTable.integer('votes').defaultTo(0);
+        commentsTable.timestamp('created_at').defaultTo(knex.fn.now());
+        commentsTable.string('body', 3000).notNullable();
+    });
+};
+
+exports.down = function(knex) {
+    console.log('dropping comments table')
+    return knex.schema.dropTable('comments');
+};
