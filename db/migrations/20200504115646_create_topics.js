36d2ed5111fae7d3e19fd7e34aa23f31864608a9
Lajanke <l.j.kenny@hotmail.com>
2020-05-04 13:01:05 +0100

    Test migrate scripts added, topics table created.

--- db/migrations/20200504115646_create_topics.js
+++ db/migrations/20200504115646_create_topics.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('topics', (topicsTable) => {
+      console.log('creating topics table');
+      topicsTable.string('slug').primary();
+      topicsTable.string('description');
+  });
 };
 
 exports.down = function(knex) {
-  
+    console.log('dropping topics table');
+  return knex.schema.dropTable('topics');
 };
+
+/*
+slug field which is a unique string that acts as the table's primary key
+description field which is a string giving a brief description of a given topic
+*/
