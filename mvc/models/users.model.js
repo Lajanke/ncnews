8222cb566754375ef5f99f1c58f3c51054fb6b0b
Lajanke <l.j.kenny@hotmail.com>
2020-05-07 14:33:46 +0100

    Refactored get /api/users/:username to return an object.

--- mvc/models/users.model.js
+++ mvc/models/users.model.js
@@ -4,11 +4,11 @@ const fetchUser = (username) => {
     return connection('users')
     .select('*')
     .where('users.username', '=', username)
-    .then((result) => {
-        if (result.length === 0) {
+    .then((res) => {
+        if (res.length === 0) {
             throw {code: 'USER NOT FOUND'};
         } else {
-            return result;
+            return res[0];
         }
     });
 };
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -74,30 +74,15 @@ describe('/api', () => {
     });
     describe('/users/:username', () => {
         describe('GET', () => {
-            test('Status 200: Only one user returned. Returns array in the body with a length of 1', () => {
-                return request(app)
-                    .get('/api/users/butter_bridge')
-                    .expect(200)
-                    .then((res) => {
-                        expect(res.body.user.length).toBe(1);
-                    });
-            });
-            test('Has properties equal to table column titles', () => {
+            test('Has properties equal to table column titles and no more', () => {
                 return request(app)
                     .get('/api/users/icellusedkars')
                     .expect(200)
                     .then((res) => {
-                        expect(res.body.user[0]).toHaveProperty('username');
-                        expect(res.body.user[0]).toHaveProperty('avatar_url');
-                        expect(res.body.user[0]).toHaveProperty('avatar_url');
-                    });
-            });
-            test('Returns an array of objects with number of keys equal to columns in topics table', () => {
-                return request(app)
-                    .get('/api/users/rogersop')
-                    .expect(200)
-                    .then((res) => {
-                        expect(Object.keys(res.body.user[0]).length).toBe(3);
+                        expect(res.body.user).toHaveProperty('username');
+                        expect(res.body.user).toHaveProperty('avatar_url');
+                        expect(res.body.user).toHaveProperty('name');
+                        expect(Object.keys(res.body.user).length).toBe(3);
                     });
             });
             test('Returns correct content for url and name', () => {
@@ -105,8 +90,8 @@ describe('/api', () => {
                     .get('/api/users/lurker')
                     .expect(200)
                     .then((res) => {
-                        expect(res.body.user[0].avatar_url).toBe('https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png');
-                        expect(res.body.user[0].name).toBe('do_nothing');
+                        expect(res.body.user.avatar_url).toBe('https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png');
+                        expect(res.body.user.name).toBe('do_nothing');
                     });
             });
         });
