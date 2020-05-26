050ddc001ba52ec9475975499ad14999c7aba761
Lajanke <l.j.kenny@hotmail.com>
2020-05-26 15:20:20 +0100

    cors added.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api.router.js');
 const { handle404s, handleErrors, handlePSQLErrors } = require('./mvc/controllers/errors.controller.js');
+const cors = require('cors');
 
 app.use(express.json());
 app.use("/api", apiRouter);
@@ -10,5 +11,7 @@ app.use('/', handle404s);
 
 app.use(handleErrors);
 app.use(handlePSQLErrors);
+
+app.use(cors());
 
 module.exports = app;
